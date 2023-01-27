39792625b1003d4d095cae22743d9cb47dbcf1bf
Adnan Alouane <alouane@usc.edu>
2023-01-27 05:03:30 +0000

    Added files

--- split.cpp
+++ split.cpp
@@ -11,13 +11,70 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <iostream>
 
 /* Add a prototype for a helper function here if you need */
+Node*& helper(Node*& in, Node*& odds, Node*& evens);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+    /* Add code here */
+    // WRITE YOUR CODE HERE
+		
+  Node* arbitrary = helper(in, odds, evens);
+
+    
+		int r_val = (*arbitrary).value % 2; // check for even values
+    if (r_val == 0) 
+		{
+        (*arbitrary).next = evens;
+				// set evens to next value
+        evens = arbitrary;
+    }
+    
+    else 
+		{
+        (*arbitrary).next = odds;	 //check for odds values
+				// set odds to next value
+        odds = arbitrary;
+    }
+
 }
 
 /* If you needed a helper function, write it here */
+Node*& helper(Node*& in, Node*& odds, Node*& evens) 
+
+// WRITE YOUR CODE HERE
+
+
+{
+	// hold val for bit
+    Node* hold;
+
+    if ((*in).next == nullptr) 
+		{
+        return in;
+    }
+    else 
+		{
+        hold = helper((*in).next, odds, evens);
+    }
+
+    // 
+    if ((*hold).value % 2 == 0) 
+		{ // evens checker
+        (*hold).next = evens;
+        evens = hold;
+        return in;
+    }
+    
+    else
+
+		 {// odds checker
+        (*hold).next = odds;
+
+        odds = hold;
+
+        return in;
+    }
+}
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,4 +1,3 @@
-foo
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
@@ -30,7 +29,8 @@ size_t ULListStr::size() const
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
-  if(ptr == NULL){
+  if(ptr == NULL)
+	{
     throw std::invalid_argument("Bad location");
   }
   *ptr = val;
@@ -57,10 +57,184 @@ std::string const & ULListStr::get(size_t loc) const
 void ULListStr::clear()
 {
   while(head_ != NULL){
-    Item *temp = head_->next;
+    Item *temp = (*head_).next;
     delete head_;
     head_ = temp;
   }
   tail_ = NULL;
   size_ = 0;
 }
+
+// pushback function
+// this function will put object to list end
+void ULListStr::push_back(const std::string &val) 
+
+{
+    
+    (*this).size_ = (*this).size_ + 1; // increment item amount by 1
+
+
+
+    // new item named i formed included 
+    Item* i = new Item(); 
+
+
+    //temp  to set to val value
+    std::string temp;
+
+    temp = val; //make temp
+    (*i).val[(*i).last] = temp;
+    
+    (*i).last = (*i).last + 1; // final object is incremented 
+
+
+
+    // check list
+    // empty or not
+    if (!((*this).head_ != nullptr)) 
+    {
+
+        // head and tails are to the begging and final items on the list 
+        // both are = to i
+        (*this).head_ = i;
+
+        // set tail_ equal to the new item
+        (*this).tail_ = i;
+
+        // final return
+        return;
+    }
+}
+
+
+
+// popback function
+// this function will take fof item from the end of list
+void ULListStr::pop_back() 
+{
+
+    (*this).size_ = (*this).size_ - 1; // list size is decremented
+
+    Item* now = (*this).tail_;
+  
+    (*this).tail_ = (*this).(*tail_).prev; // change tail value to previous tail
+
+    //search value of tail for null pointer
+    if ((*this).tail_) 
+
+    {
+      (*this).(*tail_).next = nullptr;
+    } 
+
+    if (!(*this).tail_))
+// clear 
+  { 
+   head_ = nullptr;
+   }
+
+    (*now).last = (*now).last - 1;  //decrement the final value
+
+    delete now; // remove the item 
+}
+
+// include new item to begining 
+void ULListStr::push_front(const std::string &val) 
+{
+
+    (*this).size_ = (*this).size_ + 1; // increment this size
+    Item* i = new Item();
+
+    (*i).val[(*i).last] = val;// provide the value to the new object's value
+
+
+    // check list
+    // empty or not
+    if (!((*this).head_ != nullptr)) 
+    {
+
+        // head and tails are to the begging and final items on the list 
+        // both are = to i
+        (*this).head_ = i;
+
+        // set tail_ equal to the new item
+        (*this).tail_ = i;
+
+        // final return
+        return;
+    }
+
+}
+
+// pop front 
+void ULListStr::pop_front()  // this functions clear item in front
+{ 
+    (*this).size_ =  (*this).size_ -1; // decrement
+
+    // locate the head 
+    // the one we have now
+    Item* now = (*this).head_;
+
+
+    (*this).head_ = (*this).(*head_).next; // head gets changed to next value
+
+
+  // check for null pointer value on head and tails
+    if (!(*this).tail) 
+    {
+      // set tail null pointer to tail
+      tail_ = nullptr;
+    }
+
+    if (!(*this).head) 
+    {
+      // set head null pointer to tail
+      head_ = nullptr;
+
+    }
+
+    // deallocate last item
+
+    delete now;
+}
+
+
+std::string const &ULListStr::front() const 
+{
+
+    std::string temp;
+    temp = (*this).(*head_).val[(*this).(*head_).first];
+
+    //return begining object
+    return temp;
+}
+
+
+std::string *ULListStr::getValAtLoc(size_t loc) const // location value returner func
+{
+    Item* now = (*this).head_;
+
+    for (size_t a = 1; a<loc+1; a++) 
+    {
+        now = (*now).next; // change value to new next
+        // locate if list has more amount than objects present 
+        if (now) 
+        {
+          return nullptr;
+        }
+    }
+    return (*now).val;
+}
+
+
+std::string const &ULListStr::back() const 
+{
+
+    std::string temp;
+    temp = (*this).(*tail_).val[(*this).(*tail_).last-1];
+
+    // return final object
+    return temp;
+}
+
+
+
